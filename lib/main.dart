7bfc6e5dc0737686634311106842ddc7d760039e
panjikrisnayasabfi <panjikrisnayasabfi@gmail.com>
2024-08-11 11:03:35 +0700

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hi_bank_test_app/app.dart';
 import 'package:hi_bank_test_app/app/network/network_config.dart';
+import 'package:hi_bank_test_app/app/services/firebase_options.dart';
 import 'package:http_proxy/http_proxy.dart';
 
-void main() {
-  initializeProxy();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeProxy();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ProviderScope(
@@ -20,8 +27,8 @@ void main() {
 }
 
 Future<void> initializeProxy() async {
-  WidgetsFlutterBinding.ensureInitialized();
   final HttpProxy httpProxy = await HttpProxy.createHttpProxy();
+
   HttpOverrides.global = httpProxy;
 }
 
